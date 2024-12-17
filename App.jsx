156ccda20be2adb39b2b559f1357f2dd9211ec27
Priyanshu-1408 <160210601+Priyanshu-1408@users.noreply.github.com>
2024-12-17 12:53:12 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Movie from './Components/Movie'
+import UseRef from './Components/UseRef'
+
+const App = () => {
+  return (
+    <div>
+      <Movie/>
+      <UseRef/>
+    </div>
+  )
+}
+
+export default App
